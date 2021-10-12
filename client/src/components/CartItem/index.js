0929ac52d08913d44d979974b95dca74e38727d8
Brian OConnor <boconnor20@gmail.com>
2021-10-11 21:38:19 -0500

    called dispatch and selector hooks in carrt comps

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { useStoreContext } from "../../utils/GlobalState";
+//import { useStoreContext } from "../../utils/GlobalState";
+import { useDisatch } from 'react-redux';
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
 
 const CartItem = ({ item }) => {
-
-  const [, dispatch] = useStoreContext();
+  const dispatch = useDispatch();
+  // useDispatch function and comment out Global 
+  //const [, dispatch] = useStoreContext();
 
   const removeFromCart = item => {
     dispatch({
